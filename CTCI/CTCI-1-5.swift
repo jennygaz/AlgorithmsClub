50a3dba5dbfab5ffc34ffe274b6972c23c7719ad
Jenny Gallegos <dev.jenny.ga@gmail.com>
2024-12-23 19:37:07 -0600

    [ADD] CTCI 1-5 - Edit Distance for 1

--- CTCI/CTCI-1-5.swift
+++ CTCI/CTCI-1-5.swift
@@ -4,3 +4,45 @@
  */
 /* by jennyga */
 
+@inlinable
+func isOneEditReplace(_ lhs: String, from rhs: String) -> Bool {
+    var foundDifference = false
+    for (a, b) in zip(lhs, rhs) {
+        if a != b {
+            if foundDifference { return false }
+            foundDifference = true
+        }
+    }
+    return true
+}
+
+@inlinable
+func isOneInsertionAway(_ lhs: String, from rhs: String) -> Bool {
+    var lhsChars = lhs.characters
+    var rhsChars = rhs.characters
+    var leftIndex = 0
+    var rightIndex = 0
+    while rightIndex < rhsChars.count && leftIndex < lhsChars.count {
+        if lhsChars[leftIndex] != rhsChars[rightIndex] {
+            if leftIndex != rightIndex { return false }
+            rightIndex += 1
+        } else {
+            rightIndex += 1
+            leftIndex += 1
+        }
+    }
+    return true
+}
+
+
+
+func isOneEditAway(_ lhs: String, from rhs: String) -> Bool {
+    if lhs.count == rhs.count {
+        return isOneEditReplace(lhs, from: rhs)
+    } else if lhs.count + 1 == rhs.count {
+        return isOneInsertionAway(lhs, from: rhs)
+    } else if lhs.count == rhs.count + 1 {
+        return isOneInsertionAway(rhs, from: lhs)
+    }
+    return false;
+}
